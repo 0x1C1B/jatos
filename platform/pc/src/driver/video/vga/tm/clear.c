aa9a92720e1b0e0eef9fbaeda9f4a7d902787d89
0x1C1B <32311121+0x1C1B@users.noreply.github.com>
2019-11-29 12:28:29 +0100

    Partial VGA (textmode only) driver for writing to screen

---
+++ platform/pc/src/driver/video/vga/tm/clear.c
@@ -0,0 +1,39 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 0x1C1B
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <driver/video/vga/tm/vga_tm.h>
+
+static uint16_t *video_memory = (uint16_t *) VGA_VIDEO_MEMORY;
+
+void vga_tm_clear() {
+
+    uint8_t attribute = ((VGA_TM_BLACK & 0x0F) << 4) | (VGA_TM_WHITE & 0x0F);
+	uint16_t value = 0x20 | (attribute << 8);
+
+    for(int row = 0; row < VGA_TM_ROWS; ++row) {
+
+        memsetw(video_memory + (row * VGA_TM_COLUMNS), value, VGA_TM_COLUMNS);
+    }
+}
---
+++ platform/pc/src/driver/video/vga/tm/write.c
@@ -0,0 +1,34 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2019 0x1C1B
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <driver/video/vga/tm/vga_tm.h>
+
+static uint16_t *video_memory = (uint16_t *) VGA_VIDEO_MEMORY;
+
+void vga_tm_write(size_t cell, uint8_t c, uint8_t fg, uint8_t bg) {
+
+    uint8_t attribute = ((bg & 0x0F) << 4) | (fg & 0x0F);
+    video_memory[cell] = c | (attribute << 8);
+}
